2cb45c380df4b1e3bdd8fad8300eaf1596c25597
drinoayo <81844013+drinoayo@users.noreply.github.com>
2026-09-23 18:32:16 +0100

    Update PMProGatewayBachs.php

--- src/Integrations/PMPro/PMProGatewayBachs.php
+++ src/Integrations/PMPro/PMProGatewayBachs.php
@@ -57,10 +57,6 @@ final class PMProGateway_Bachs extends PMProGateway {
 	 * @return bool
 	 */
 	public function process( &$order ) {
-		if ( ! $order instanceof MemberOrder ) {
-			return false;
-		}
-
 		return PMProGatewayRuntime::process( $order );
 	}
 }
